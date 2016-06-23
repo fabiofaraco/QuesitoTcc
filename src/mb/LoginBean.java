745556f035d5e9adbc6555842a3d12c06f0b8411
fabiofaraco <f.faraco27@gmail.com>
2016-06-22 23:38:43 -0300

    desenvolvimento da funcionalidade que guarda o usuario na sessão

--- src/mb/LoginBean.java
+++ src/mb/LoginBean.java
@@ -6,6 +6,7 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import bean.Usuario;
 import dao.UsuarioDao;
@@ -16,21 +17,25 @@ public class LoginBean {
 	private Usuario usuario = new Usuario();
 	
 	public String logar() {
+		UsuarioDao dao = new UsuarioDao();
 		
-		if(this.usuario.getEmail().equals("admin") && this.usuario.getSenha().equals("admin")) {
+		if(usuario.getEmail().equals("admin") && 
+				usuario.getSenha().equals("admin")) {
 			return "menu";
 		}
 		
-		UsuarioDao dao = new UsuarioDao();
-		
 		usuario = dao.buscarUsuarioLogin(this.usuario.getEmail(), this.usuario.getSenha());
-		
 		if(usuario == null) {
 			 FacesContext.getCurrentInstance()
 			 			.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário não encontrado!", "Erro no Login!"));
 			
 			return null;
 		} else {
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			ExternalContext externalContext = facesContext.getExternalContext();
+			HttpSession session = (HttpSession)externalContext.getSession(false);
+			session.setAttribute("usuario", this.usuario );
+			
 			return "menu";
 		}
 	}
